a384c10254d99828002fda6c6b8e3aa75c866f20
ElYizuz123 <jose.jesus.zarate.garcia@outlook.com>
2024-02-10 16:27:40 -0600

    Just a test

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const LogIn = () => {
   return (
-    <div className='w-4/12 h-5/6 bg-slate-600 rounded-3xl bg-grayFG'>
+    <div className='w-4/12 h-5/6 bg-slate-600 rounded-3xl bg-grayFG '>
         LogOut
     </div>
   )
