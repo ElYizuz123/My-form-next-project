02b95409f310a925d6ba21f8a0e34a2b7bc9a285
ElYizuz123 <jose.jesus.zarate.garcia@outlook.com>
2024-02-10 22:07:36 -0600

    size changes

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -12,21 +12,21 @@ const LogIn = () => {
         console.log(formData)
     }
   return (
-    <div className='w-96 h-96 bg-slate-600 rounded-3xl bg-grayFG'>
-        <form onSubmit={onSubmit}>
-            <div className='w-full h-3 mt-10 flex justify-center items-center'>
+    <div className='w-35 h-60 bg-slate-600 rounded-3xl bg-grayFG'>
+        <form onSubmit={onSubmit} className='h-full w-full'>
+            <div className='w-full h-3 mt-8 flex justify-center items-center'>
                 <p className='font-bold text-xl text-white'>LOGIN</p>
             </div>
-            <div className='w-full h-16 mt-8 flex justify-center items-center'>
-                <input onChange={handleOnChange} type='email' placeholder='E-mail' className='w-11/12 h-10  px-4 text-black rounded-xl' 
+            <div className='w-full h-10 mt-10 flex justify-center items-center'>
+                <input onChange={handleOnChange} type='email' placeholder='E-mail' className='w-11/12 h-100  px-4 text-black rounded-xl' 
                 name='email' id='email'></input>
             </div>
-            <div className='w-full h-16 mt-14 flex justify-center items-center'>
-                <input onChange={handleOnChange} type='password' placeholder='Password' className='w-11/12 h-10  px-4 text-black rounded-xl' 
+            <div className='w-full h-10 mt-20 flex justify-center items-center'>
+                <input onChange={handleOnChange} type='password' placeholder='Password' className='w-11/12 h-100  px-4 text-black rounded-xl' 
                 name='password' id='password'></input>
             </div>
-            <div className='w-full h-16 mt-8 flex justify-end items-end'>
-                <button className='w-20 h-10 bg-buttonC mr-6 rounded-xl text-white' type='submit'>
+            <div className='w-full h-10 mt-10 flex justify-end items-end'>
+                <button className='w-20 h-100 bg-buttonC mr-6 rounded-xl text-white' type='submit'>
                     <p>Login</p>
                 </button>
             </div>
